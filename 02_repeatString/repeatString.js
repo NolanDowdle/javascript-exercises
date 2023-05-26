d37f0d062b8d28c11349352aff31b7eaeea7b07a
Full Name <njdowdle@gmail.com>
2023-05-27 02:14:23 +0700

    finished exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    tempString = "";
+    for (var i = 0; i < num; i++) {
+        tempString = tempString + string
+    }
+    if(num < 0) {
+        return 'ERROR';
+    }
+    return tempString;
 };
 
 // Do not edit below this line
