622f48071e66ac2f8b5148f6d333f1fa25617276
Full Name <njdowdle@gmail.com>
2023-05-28 20:37:22 +0700

    fixed 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(original) {
+    var reversed = "";
+    for (var i = original.length; i >= 0; i--) {
+        reversed += original.charAt(i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
